f44b65053508aa4e91050967e98cde43287a6126
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-05-18 12:32:35 +0530

    content update

--- src/main/java/com/mgunawardhana/springbootvirtualthreads/SpringBootVirtualThreadsApplication.java
+++ src/main/java/com/mgunawardhana/springbootvirtualthreads/SpringBootVirtualThreadsApplication.java
@@ -10,4 +10,44 @@ public class SpringBootVirtualThreadsApplication {
         SpringApplication.run(SpringBootVirtualThreadsApplication.class, args);
     }
 
+    /*
+     * Customizes the Tomcat server's protocol handler to use a virtual thread executor.
+     *
+     * <p>This method is annotated with {@code @Bean}, which means it will be instantiated,
+     * assembled, and managed by Spring IoC container. Inside this method, a
+     * {@code TomcatProtocolHandlerCustomizer} object is returned. This is a functional
+     * interface in Spring Boot that allows you to customize the protocol handler used by
+     * the Tomcat server.</p>
+     *
+     * <p>The lambda expression provided as the implementation of the
+     * {@code TomcatProtocolHandlerCustomizer} interface sets the executor of the protocol
+     * handler to a new virtual thread executor. This executor creates a new virtual thread
+     * for each task, which can be more efficient than using a fixed pool of OS threads,
+     * especially when dealing with a large number of tasks that perform blocking operations.
+     * This is part of the Project Loom feature in Java, which aims to make concurrency
+     * easier by introducing lightweight, efficient threads managed by the Java runtime
+     * rather than the operating system.</p>
+     *
+     * @return a {@code TomcatProtocolHandlerCustomizer} that sets the executor of the
+     * protocol handler to a new virtual thread executor
+     */
+
+    /*
+     * We don't need this bean to enable virtual threads in Spring Boot because
+     * we can use this `spring.threads.virtual.enabled=true`.
+     *
+     * Spring Boot 2.6.0+ automatically enables virtual threads for Tomcat,
+     * so we can remove this bean.
+     *
+     * If we want to enable virtual threads in Spring Boot 2.5.x or earlier versions,
+     * we can use this bean.
+     */
+
+//    @Bean
+//    public TomcatProtocolHandlerCustomizer<?> tomcatProtocolHandlerCustomizer() {
+//        return (protocolHandler) -> {
+//            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+//        };
+//    }
+
 }
--- src/main/java/com/mgunawardhana/springbootvirtualthreads/controller/ProductController.java
+++ src/main/java/com/mgunawardhana/springbootvirtualthreads/controller/ProductController.java
@@ -28,19 +28,25 @@ public class ProductController {
      * Each platform thread corresponds to a physical thread of execution and is scheduled by the operating system's scheduler.
      * They are heavyweight in nature, meaning they consume more resources and have a higher context-switching cost.
      * In Java, the `java.lang.Thread` class represents a platform thread.
-     * <p>
+     * if we don't use virtual threads, the output will be like below:
+     * 2024-05-18T11:52:13.730+05:30 INFO 2744 --- [spring-boot-virtual-threads] [nio-8080-exec-1] c.m.s.controller.ProductController
+     * : Thread info: Thread[#43,http-nio-8080-exec-1,5,main], name:http-nio-8080-exec-1
+     *
      * Virtual Threads:
      * Virtual threads, also known as lightweight or user-mode threads, are threads that are managed by the runtime or a library,
      * not by the operating system. They are lightweight in nature, meaning they consume fewer resources and have a lower
      * context-switching cost compared to platform threads. Virtual threads can be mapped to a smaller set of platform threads,
      * allowing a large number of virtual threads to be run concurrently. In Java, virtual threads are introduced as a part of
      * Project Loom (which is not yet part of any official Java release as of JDK 17).
+     * if we enable virtual threads, the output will be like below:
+     * 2024-05-18T11:55:16.489+05:30 ERROR 3980 --- [spring-boot-virtual-threads] [omcat-handler-0] c.m.s.controller.ProductController
+     * : Thread info: VirtualThread[#50,tomcat-handler-0]/runnable@ForkJoinPool-1-worker-1, name:tomcat-handler-0
      */
 
     @GetMapping
     public List<Product> getProducts() throws InterruptedException {
         Thread.sleep(1000);
-        log.info("Thread info: {}", Thread.currentThread());
+        log.error("Thread info: {}, name:{} ", Thread.currentThread(), Thread.currentThread().getName());
         return productRepository.findAll();
     }
 }
